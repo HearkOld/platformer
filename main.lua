e1817539bcbaef8cf0edba7796a645b9e8b5bfab
Heark <hearkofsorcerey@gmail.com>
2014-12-02 20:04:44 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,16 @@
+--
+-- Simple Platformer Game (C)opyright Heark 2014
+--
+
+-- Require Files
+require "player"
+
+-- Main
+
 function love.load()
 love.graphic.getBackgroundColor(255, 255, 255)
+-- Loading Classes
+player.load()
 end
 function love.update(dt)
 
